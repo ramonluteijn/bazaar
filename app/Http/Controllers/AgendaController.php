81f25c54135a18b5301aed2a87370c6e0c5a6d5d
ramon luteijn <ramon@intractief.nl>
2025-03-17 20:28:22 +0100

    #9 hired ads agenda

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -4,19 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Advertisement;
 use App\Models\Order;
+use Illuminate\Http\Request;
 
 class AgendaController
 {
-    public function index()
-    {
-        $orders = Order::query()->whereHas('orderDetails.advertisement', function ($query) {
-            $query->where('type', 'hire');
-        })->where('user_id', auth()->id())->with('orderDetails.advertisement')->paginate(1, ['*'], 'ordersPage');
+    private $types = ['hire' => 'hire', 'sale' => 'sale', 'bid' => 'bid'];
 
-        if(!auth()->user()->hasRole("user")) {
-            $advertisements = Advertisement::query()->where('user_id', auth()->id())->paginate(1, ['*'], 'adsPage');
+    public function index(Request $request)
+    {
+        $tables = [
+            'orders' => 'orders',
+        ];
+        if(!auth()->user()->hasRole('user')) {
+            $tables['advertisements'] = 'advertisements';
         }
 
-        return view('agenda.index', ['orders' => $orders, 'advertisements' => $advertisements ?? null]);
+        if($request->has('selectTable') && $request->selectTable == 'orders') {
+            $orders = Order::query()->whereHas('orderDetails.advertisement', function ($query) {
+                $query->where('type', 'hire');
+            })->where('user_id', auth()->id())->with('orderDetails.advertisement')->paginate(10, ['*'], 'ordersPage');
+        }
+        if($request->has('selectTable') && $request->selectTable == 'advertisements') {
+            if ($request->selectType == '') {
+                $advertisements = Advertisement::query()->where('user_id', auth()->id())->paginate(10, ['*'], 'adsPage');
+            }
+            else {
+                $advertisements = Advertisement::query()->where('user_id', auth()->id())->where('type', $request->selectType)->paginate(10, ['*'], 'adsPage');
+            }
+        }
+
+        return view('agenda.index', ['orders' => $orders ?? null, 'advertisements' => $advertisements ?? null, 'tables' => $tables, 'types' => $this->types]);
     }
 }
--- resources/views/components/forms/input-select.blade.php
+++ resources/views/components/forms/input-select.blade.php
@@ -1,29 +1,51 @@
-@props(["name", "required" => false, "list" => [], "enum" => null, "value", "class" => ""])
+@props([
+  "name",
+  "required" => false,
+  "list" => [],
+  "enum" => null,
+  "value" => "",
+  "class" => "",
+  "label" => "",
+  "default" => "",
+  'onchange' => ''
+])
 
 <div class="mb-4">
-    <label class="block text-gray-700 text-sm font-bold mb-2" for="{{ $name }}">
-        {{ \Illuminate\Support\Str::of($name)->kebab()->replace("_", " ")->ucfirst() }}
+    <label class="block text-gray-700 text-sm font-bold mb-1" for="{{ $name }}">
+        {{ \Illuminate\Support\Str::of($label)->kebab()->replace("-", " ")->ucfirst() }}
         @if ($required)
-            <span class="text-red-500">*</span>
+            <span class="">*</span>
         @endif
     </label>
-    <select name="{{ $name }}" id="{{ $name }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline {{ $class }}"{{ $required ? "required" : "" }}>
-        <option value="">Select a {{ $name }}</option>
-        @if ($enum)
-            @foreach ($enum::cases() as $item)
-                <option value="{{ $item->value }}"{{ $item->name == $value ? "selected" : "" }}>
-                    {{ $item->name }}
-                </option>
-            @endforeach
-        @else
-            @foreach ($list as $key => $item)
-                <option value="{{ $key }}" {{ $item == $value ? "selected" : "" }}>
-                    {{ $item }}
-                </option>
-            @endforeach
-        @endif
-    </select>
+
+    <div class="relative">
+        <select
+            onchange="{{ $onchange }}"
+            name="{{ $name }}"
+            id="{{ $name }}"
+            class="border border-gray-400 bg-white rounded-md w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 hover:border-gray-500 appearance-none pr-8 {{ $class }}"
+            {{ $required ? "required" : "" }}
+        >
+            <option value="">{{ $default != '' ? $default : 'Selecteer een '.strtolower($label) }}</option>
+            @if ($enum)
+                @foreach ($enum::cases() as $item)
+                    <option
+                        value="{{ strtoupper($item->value) }}"
+                        {{ $item->value == $value ? "selected" : "" }}
+                    >
+                        {{ __($item->value) }}
+                    </option>
+                @endforeach
+            @else
+                @foreach ($list as $key => $item)
+                    <option value="{{ $key }}" {{ $key == $value ? "selected" : "" }}>
+                        {{ ucfirst(strtolower(__($item))) }}
+                    </option>
+                @endforeach
+            @endif
+        </select>
+    </div>
     @error($name)
-    <span class="text-red-500 text-xs italic">{{ $message }}</span>
+    <span class="text-xs italic">{{ $message }}</span>
     @enderror
 </div>
